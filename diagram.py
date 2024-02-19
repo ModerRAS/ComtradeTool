71fddc8a256c5e3e8a2a2eabbb73608f22e9ef09
ModerRAS <adqewsx@gmail.com>
2024-02-19 16:30:25 +0800

    完成程序编写

--- diagram.py
+++ diagram.py
@@ -5,6 +5,7 @@ from comtrade import Comtrade
 import csv
 import os
 import codecs
+from pywebio.output import *
 
 直流场电流模拟量 = [
     "IDEL1",
@@ -187,9 +188,10 @@ def get_analog_from_file(filepath):
     def _get_analog_from_file(字段, 量, Child="Child0"):
         data_list = []
         for i in 字段:
+            # put_markdown("读取字段 {}".format(i))
             files = filter_files(filepath, [get_filename_keyword(i), Child])
             if len(files) <= 0:
-                log_list.append("cannot find {}".format(i))
+                put_markdown("cannot find {}".format(i))
                 continue
             path_without_extension, extension = os.path.splitext(files[0])
             rec = load_diagram(path_without_extension)
@@ -256,18 +258,20 @@ def convert_data_to_csv_2(data, csv_file_path):
     print(f"CSV 文件已保存至 {csv_file_path}")
 
 
-def find_diagram(filepath: str):
+def find_diagram(filepath: str, csv_path: str):
     start_time = time.time()
     log_list.clear()
     # 直流场电流模拟量
     func_get_analog_from_file = get_analog_from_file(filepath)
+    put_markdown("读取直流场电流模拟量")
     直流场电流 = func_get_analog_from_file(直流场电流模拟量_字段名, 直流场电流模拟量)
+    put_markdown("读取直流场电压模拟量")
     直流场电压 = func_get_analog_from_file(直流场电压模拟量_字段名, 直流场电压模拟量)
     直流场电压_PCP_CCP = func_get_analog_from_file(直流场电压模拟量_PCP_CCP_字段名, 直流场电压模拟量_PCP_CCP)
+    put_markdown("读取换流变模拟量")
     换流变 = func_get_analog_from_file(换流变模拟量_字段名, 换流变模拟量, Child="Child2")
 
-    print(直流场电压)
-    convert_data_to_csv_2(直流场电压 + 直流场电流 + 换流变, '直流场电压.csv')
+    put_markdown("开始保存为CSV")
 
     data_list = [
         convert_data_to_csv_style("直流场电流", 直流场电流),
@@ -275,28 +279,12 @@ def find_diagram(filepath: str):
         convert_data_to_csv_style("直流场电压", 直流场电压_PCP_CCP),
         convert_data_to_csv_style("换流变", 换流变)
     ]
-    save_to_csv(data_list, "data.csv")
-    print(直流场电流)
+    save_to_csv(data_list, csv_path)
     end_time = time.time()
     # 计算时间差
     elapsed_time = end_time - start_time
     print(f"程序运行时间为：{elapsed_time} 秒")
     pass
-
-
-def write_to_csv(filename: str, output_analog):
-    f = open(filename, 'w', encoding='UTF8', newline='')
-
-    # create the csv writer
-    writer = csv.writer(f)
-
-    # write a row to the csv file
-    for row in use_analog_list:
-        for per in output_analog:
-            if per["name"] == row:
-                writer.writerow([per["name"], per["value"]])
-
-    f.close()
 
 if __name__ == '__main__':
     find_diagram(r"C:\WorkSpace\Recoder\20231006test")
--- main.py
+++ main.py
@@ -1,14 +1,38 @@
+import os
+import random
+import shutil
+import string
+import time
+import zipfile
 from pywebio import start_server
 from pywebio.input import *
 from pywebio.output import *
 from pywebio.session import info as session_info
 
+from diagram import find_diagram
 
+def unzip_file(zip_file_path, extract_to_folder):
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to_folder)
+
+def generate_random_string(length):
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(length))
 
 def t(eng, chinese):
     """return English or Chinese text according to the user's browser language"""
     return chinese if 'zh' in session_info.user_language else eng
 
+def delete_file_or_folder(path):
+    """删除文件或文件夹"""
+    if os.path.isfile(path):
+        os.remove(path)
+        print(f'文件 {path} 已删除。')
+    elif os.path.isdir(path):
+        shutil.rmtree(path)
+        print(f'文件夹 {path} 及其内容已删除。')
+    else:
+        print(f'路径 {path} 不是有效的文件或文件夹路径。')
 
 def main():
     """分析波形的简单应用
@@ -18,11 +42,27 @@ def main():
     """, """
 # 模拟量检查工具
 用于自动生成小定值零飘检查需要的表格文件
+将下载的内置录波谱图文件夹打包成zip文件，然后上传，请勿更改导出文件名
+文件夹层级任意
     """))
 
-    info = input_group(t('', '波形路径：'), [
-        input(t("Your Comtrade file folder", "请输入从后台导出的波形路径"), name="filepath"),
-    ])
+    f = file_upload("上传波形文件压缩包", accept="application/zip")
+    start_time = time.time()
+    put_markdown("处理中。。。。。。")
+    random_name = generate_random_string(10)
+    open('/tmp/'+ random_name + ".zip", 'wb').write(f['content'])
+    unzip_file('/tmp/'+ random_name + ".zip", '/tmp/'+ random_name)
+    find_diagram('/tmp/'+ random_name, csv_path='/tmp/'+ random_name + ".csv")
+    content = open('/tmp/'+ random_name + ".csv", 'rb').read()
+    put_markdown("清理临时文件")
+    delete_file_or_folder('/tmp/'+ random_name + ".zip")
+    delete_file_or_folder('/tmp/'+ random_name)
+    delete_file_or_folder('/tmp/'+ random_name + ".csv")
+    end_time = time.time()
+    # 计算时间差
+    elapsed_time = end_time - start_time
+    put_markdown(f"程序运行时间为：{elapsed_time} 秒")
+    put_file(random_name + ".csv", content, '点击下载CSV文件')
 
 
 if __name__ == '__main__':
