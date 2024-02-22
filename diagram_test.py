4f28b4c78561ac6c894fec6f9fda233eb8b72f1e
ModerRAS <adqewsx@gmail.com>
2024-02-22 10:31:06 +0800

    修改为比对csv内容

--- diagram_test.py
+++ diagram_test.py
@@ -1,14 +1,18 @@
+import csv
 import unittest
 
 from diagram import find_diagram
 
 class TestDiagram(unittest.TestCase):
-
+    def compare_csv(self, file1, file2):
+        with open(file1, newline='') as f1, open(file2, newline='') as f2:
+            reader1 = csv.reader(f1)
+            reader2 = csv.reader(f2)
+            for row1, row2 in zip(reader1, reader2):
+                if row1 != row2:
+                    return False
+            # Check if both files have the same number of rows
+            return next(reader1, None) is None and next(reader2, None) is None
     def test_find_diagram(self):
         find_diagram("testdata/diagram1", "testdata/diagram1.csv")
-        with open("testdata/diagram1.csv", 'rb') as f:
-            data = f.read()
-        with open("testdata/result1.csv", 'rb') as f:
-            example_result = f.read()
-        for i,d in enumerate(data):
-            self.assertEqual(d, example_result[i])
+        self.assertTrue(self.compare_csv("testdata/diagram1.csv", "testdata/result1.csv"))
