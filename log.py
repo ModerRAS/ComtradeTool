5a2bb5a8b83d9ae161d34eaeaded889eea615dc1
ModerRAS <adqewsx@gmail.com>
2024-04-24 12:16:01 +0800

    优化日志打印

--- log.py
+++ log.py
@@ -2,10 +2,11 @@ from pywebio.output import *
 
 import sys
 
-def print_log(log_str: str, progress=-1):
+def print_log(log_str: str, progress=-1.0):
     if 'unittest' in sys.modules.keys():
         print(log_str)
-    elif progress!=-1:
+    elif progress>=0:
         set_progressbar("Progress", progress)
+        print(log_str)
     else:
         put_markdown(log_str)
